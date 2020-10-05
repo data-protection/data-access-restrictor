3053b100e48116f3180af948c80278d28a6c3437
Gerold Busch <gerold.busch@gmail.com>
2020-10-05 21:02:19 +0200

    more debugging

--- start.sh
+++ start.sh
@@ -4,10 +4,9 @@ set -e
 
 mkdir -p build/config
 echo '{}' > build/config/rate_limited_uris.json
+chmod -R +w build/config
 mkdir -p build/state
 echo '{}' > build/state/last_access.json
-
-ls -l /
-ls -l /config
+chmod -R +w build/state
 
 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose up --build "$@"
